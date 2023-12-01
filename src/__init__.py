2271c33320dd61579fa00488ae0b4804f284b713
RabbikDhuka <solankiyash922@gmail.com>
2023-12-01 13:15:53 -0500

    part 3

--- src/__init__.py
+++ src/__init__.py
@@ -1,18 +1,30 @@
 # src/__init__.py
-import os  # new
-import sys
-
-
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
+from flask_sqlalchemy import SQLAlchemy  # new
 
 # instantiate the app
 app = Flask(__name__)
 api = Api(app)
 # set config
-print(app.config, file=sys.stderr)
-app_settings = os.getenv("APP_SETTINGS")  # new
-app.config.from_object(app_settings)  # new
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
+# instantiate the db
+db = SQLAlchemy(app)  # new
+
+
+# model
+class User(db.Model):  # new
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 class Ping(Resource):
