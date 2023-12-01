31b8d679711165872217230b3b340c4f9860739f
RabbikDhuka <solankiyash922@gmail.com>
2023-12-01 13:02:30 -0500

    part 2

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,8 @@
 # src/__init__.py
+import os  # new
+import sys
+
+
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -6,12 +10,14 @@ from flask_restx import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 # set config
-app.config.from_object("src.config.DevelopmentConfig")  # new
+print(app.config, file=sys.stderr)
+app_settings = os.getenv("APP_SETTINGS")  # new
+app.config.from_object(app_settings)  # new
 
 
 class Ping(Resource):
     def get(self):
-        return {"status": "success", "message": "pong!!"}
+        return {"status": "success", "message": "pong!"}
 
 
 api.add_resource(Ping, "/ping")
